55b62b663d56977bc914413cd371a9dd8103e521
krung2 <jungbin4337@dgsw.hs.kr>
2021-06-03 12:28:33 +0900

    Add: Global Pipe

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PORT } from './config/dotenv';
@@ -6,6 +7,7 @@ import { ErrorFilter } from './fillters/error.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bodyParser: true, cors: true });
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new ErrorFilter());
   await app.listen(PORT);
 }
