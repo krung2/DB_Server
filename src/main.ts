cdbaab5936558613833775a08c4eb905080d1034
krung2 <jungbin4337@dgsw.hs.kr>
2021-06-03 12:27:47 +0900

    Add: filters

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PORT } from './config/dotenv';
+import { ErrorFilter } from './fillters/error.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { bodyParser: true, cors: true });
+  app.enableCors();
+  app.useGlobalFilters(new ErrorFilter());
+  await app.listen(PORT);
 }
 bootstrap();
