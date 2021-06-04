850cd6e1a81dc0a8751b8018d5a506e033722894
krung2 <jungbin4337@dgsw.hs.kr>
2021-06-04 17:49:08 +0900

    Add: addHate & delLike

--- src/hate/hate.module.ts
+++ src/hate/hate.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import Hate from 'src/models/hate.entity';
 import Post from 'src/models/post.entity';
+import { PostService } from 'src/post/post.service';
 import { HateController } from './hate.controller';
 import { HateService } from './hate.service';
 
@@ -10,6 +11,9 @@ import { HateService } from './hate.service';
     TypeOrmModule.forFeature([Hate, Post]),
   ],
   controllers: [HateController],
-  providers: [HateService]
+  providers: [
+    HateService,
+    PostService
+  ]
 })
 export class HateModule { }
--- src/hate/hate.service.ts
+++ src/hate/hate.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { IUser } from 'src/libs/interface/IUser';
 import Hate from 'src/models/hate.entity';
+import { PostService } from 'src/post/post.service';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -8,8 +10,36 @@ export class HateService {
 
   constructor(
     @InjectRepository(Hate)
-    private hateRepository: Repository<Hate>
+    private hateRepository: Repository<Hate>,
+    private readonly postService: PostService,
   ) { }
 
+  async addLike(tokenUser: IUser, postIdx: number): Promise<void> {
 
+    const createLike = this.hateRepository.create();
+
+    createLike.userId = tokenUser.name;
+
+    await this.hateRepository.save(createLike);
+
+  }
+
+  async delLike(tokenUser: IUser, postIdx: number): Promise<void> {
+
+    await this.postService.getPostByIdx(postIdx);
+
+    const { name } = tokenUser;
+
+    const hate: Hate | undefined = await this.hateRepository.createQueryBuilder()
+      .where('user_id = :name', { name })
+      .where('post_idx = :postIdx', { postIdx })
+      .getOne();
+
+    if (hate === undefined) {
+
+      throw new NotFoundException('없는 좋아요 입니다');
+    }
+
+    await this.hateRepository.delete(hate);
+  }
 }
