9856b702df7af08a3d1ad0331b854199bc45eaf5
krung2 <jungbin4337@dgsw.hs.kr>
2021-06-04 17:50:59 +0900

    Add: hate controller

--- src/hate/hate.controller.ts
+++ src/hate/hate.controller.ts
@@ -1,4 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Param, Post, UseGuards } from '@nestjs/common';
+import { Token } from 'src/libs/decorator/token.decorator';
+import { IUser } from 'src/libs/interface/IUser';
+import { returnLib } from 'src/libs/return.lib';
+import AuthGaurd from 'src/middleware/auth.middleware';
 import { HateService } from './hate.service';
 
 @Controller('hate')
@@ -8,5 +12,27 @@ export class HateController {
     private readonly hateService: HateService,
   ) { }
 
+  @Post('/:idx')
+  @UseGuards(new AuthGaurd())
+  async addLike(
+    @Token() tokenUser: IUser,
+    @Param('idx') idx: number,
+  ) {
 
+    await this.hateService.addLike(tokenUser, idx);
+
+    return returnLib(201, '좋아요 성공');
+  }
+
+  @Delete('/:idx')
+  @UseGuards(new AuthGaurd())
+  async delLike(
+    @Token() tokenUser: IUser,
+    @Param('idx') idx: number,
+  ) {
+
+    await this.hateService.delLike(tokenUser, idx);
+
+    return returnLib(200, '좋아요 삭제 성공');
+  }
 }
